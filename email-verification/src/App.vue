5c55389348e06c1132214d61bee8a803e7a7d3b2
王艺霖 <wangyilin13@meituan.com>
2023-09-22 15:54:48 +0800

    rebuild script

--- email-verification/src/App.vue
+++ email-verification/src/App.vue
@@ -1,54 +1,3 @@
-<script setup>
-import { ref, watch, computed } from 'vue'
-const email = ref('') //TODo ref? 什么时候可以省略value?
-const uid = ref('')
-const isValid = ref(false)
-const inputBorderColor = ref('red')
-const DEFAULT_CONNECT_SUFFIX = '@connect.hku.hk'
-const DEFAULT_SUFFIX = '@hku.hk'
-const MIN_LENGTH = Math.min(
-  DEFAULT_CONNECT_SUFFIX.length,
-  DEFAULT_SUFFIX.length
-)
-watch(email, (newInput, oldInput) => {
-  var prefix = ''
-  if (newInput.length <= MIN_LENGTH) {
-    isValid.value = false
-  } else if (
-    // 后缀检查
-    !newInput.endsWith(DEFAULT_CONNECT_SUFFIX) &&
-    !newInput.endsWith(DEFAULT_SUFFIX)
-  ) {
-    isValid.value = false
-  } else {
-    //前缀检查
-    var prefixEndPosition = email.value.indexOf('@') //后缀合法，不用判断prefixEndPosition的边界
-    prefix = email.value.substring(0, prefixEndPosition)
-    isValid.value = true //假设合法，进行最后一步校验
-    for (var i = 0; i < prefix.length; i++) {
-      var ascCode = prefix.charCodeAt(i)
-
-      //数字的ascii码 [48, 57], 大写字母的ascii码 [65, 90], 小写字母的ascii码 [97，122]
-      if (
-        ascCode < 48 ||
-        (ascCode > 57 && ascCode < 65) ||
-        (ascCode > 90 && ascCode < 97) ||
-        ascCode > 122
-      ) {
-        isValid.value = false
-        break
-      }
-    }
-  }
-  if (!isValid.value) {
-    inputBorderColor.value = 'red'
-  } else {
-    inputBorderColor.value = 'green' //输入框命中焦点会变红
-    uid.value = prefix
-  }
-})
-</script>
-
 <template>
   <div class="container">
     <p>
@@ -65,6 +14,65 @@ watch(email, (newInput, oldInput) => {
     <p id="id-text">{{ uid }}</p>
   </div>
 </template>
+
+<script>
+export const DEFAULT_CONNECT_SUFFIX = '@connect.hku.hk'
+export const DEFAULT_SUFFIX = '@hku.hk'
+export const MIN_LENGTH = Math.min(
+  DEFAULT_CONNECT_SUFFIX.length,
+  DEFAULT_SUFFIX.length
+)
+export default {
+  name: 'app',
+  data() {
+    return {
+      email: '',
+      uid: '',
+      isValid: false,
+      inputBorderColor: 'red',
+    }
+  },
+  watch: {
+    verify(newInput, oldInput) {
+      var prefix = ''
+      if (newInput.length <= MIN_LENGTH) {
+        isValid.value = false
+      } else if (
+        // 后缀检查
+        !newInput.endsWith(DEFAULT_CONNECT_SUFFIX) &&
+        !newInput.endsWith(DEFAULT_SUFFIX)
+      ) {
+        isValid.value = false
+      } else {
+        //前缀检查
+        var prefixEndPosition = email.value.indexOf('@') //后缀合法，不用判断prefixEndPosition的边界
+        prefix = email.value.substring(0, prefixEndPosition)
+        isValid.value = true //假设合法，进行最后一步校验
+        for (var i = 0; i < prefix.length; i++) {
+          var ascCode = prefix.charCodeAt(i)
+
+          //数字的ascii码 [48, 57], 大写字母的ascii码 [65, 90], 小写字母的ascii码 [97，122]
+          if (
+            ascCode < 48 ||
+            (ascCode > 57 && ascCode < 65) ||
+            (ascCode > 90 && ascCode < 97) ||
+            ascCode > 122
+          ) {
+            isValid.value = false
+            break
+          }
+        }
+      }
+      if (!isValid.value) {
+        inputBorderColor.value = 'red'
+      } else {
+        inputBorderColor.value = 'green'
+        uid.value = prefix
+      }
+    },
+  },
+}
+</script>
 
 <style>
 .container {
